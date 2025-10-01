57bda99790add8def4970dcea549f623ef78e658
quentintyr <quentin.tyr@gmail.com>
2025-10-01 12:24:44 +0200

    REF | added constants file from extender to global constans file

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -23,20 +23,32 @@ namespace Constants {
         inline constexpr Motor kMotorElevator = MOTOR_1;
 
         // ultra sonic right
-        inline constexpr int RIGHT_TRIG_PORT = 15;
-        inline constexpr int RIGHT_ECHO_PORT = 16;
+        inline constexpr int RIGHT_TRIG_PORT = 2;
+        inline constexpr int RIGHT_ECHO_PORT = 3;
         // ultra sonic left
-        inline constexpr int LEFT_TRIG_PORT = 8;
-        inline constexpr int LEFT_ECHO_PORT = 9;
+        inline constexpr int LEFT_TRIG_PORT = 0;
+        inline constexpr int LEFT_ECHO_PORT = 1;
 
         // sensor update rate
         inline constexpr int SENSOR_UPDATE_RATE = 25;
 
+        // elevator 
         inline constexpr int GRIPPER_TILT_PORT = 10;
         inline constexpr int GRIPPER_OPEN_PORT = 11;
 
         inline constexpr int ELEVATOR_MOVE_PORT = 12;
         inline constexpr int ELEVEVATOR_UPDOWN_PORT = 13;
         inline constexpr int ELEVATOR_ROTATE_PORT = 14;
+
+
+        // extender
+        inline constexpr int EXTENDER_SERVO_PORT = 18;
+
+        inline constexpr int LIMIT_SWITCH_STOP_BACK_PORT = 11;
+        inline constexpr int LIMIT_SWITCH_STOP_FRONT_PORT = 10;
+
+        inline constexpr double EXTENDER_DRIVE_FORWARD = 0.0;
+        inline constexpr double EXTENDER_DRIVE_BACKWARD = 1.0;
+        inline constexpr double EXTENDER_DRIVE_STOP = 0.5;
 
 }
